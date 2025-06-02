608453a9cdf6333c2349eb16487c5b0926f70980
alot80323 <alot80323@gmail.com>
2025-06-01 23:17:58 -0600

    Update install.mjs

--- .husky/install.mjs
+++ .husky/install.mjs
@@ -1,4 +1,4 @@
-if (process.env.NODE_ENV === 'production' || process.env.CI === 'true')
+if (process.env.NODE_ENV === 'production' || process.env.CI === 'false')
   process.exit(0);
 
 const husky = (await import('husky')).default;
